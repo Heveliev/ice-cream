5c0c689106195f887eb08cd2f6d4131fdcefaeda
Heveliev <109628656+Heveliev@users.noreply.github.com>
2022-10-11 10:57:46 +0000

    Deploying to gh-pages from @ Heveliev/ice-cream@a2484365a28e9074cdf1e77b2349f559af540774 🚀

---
+++ index.a7dc339d.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-fran]"),closeModalBtn:document.querySelector("[data-modal-close-fran]"),modal:document.querySelector("[data-modal-fran]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a7dc339d.js.map
