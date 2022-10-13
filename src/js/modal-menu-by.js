21d34dafa64779e5ba8216e90a3d526b2bd71408
YevgeniiKoval <109030577+YevgeniiKoval@users.noreply.github.com>
2022-10-13 18:44:17 +0300

    by-n

---
+++ src/js/modal-menu-by.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-mob-buy-n]"),
+      closeModalBtn: document.querySelector("[data-modal-close-mob-buy-n]"),
+      modal: document.querySelector("[data-modal-buy-n]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
