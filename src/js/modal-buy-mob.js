a98e001f0e3a0f7177a4a5c3c8130f7c129a8f5c
Heveliev-destress <tolik05.03@ukr.net>
2022-10-14 14:40:09 +0300

    fix

--- src/js/modal-buy-mob.js
+++ src/js/modal-buy-mob.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open-mob-buy-n]"),
       closeModalBtn: document.querySelector("[data-modal-close-mob-buy-n]"),
-      modal: document.querySelector("[data-modal-buy-n]"),
+      modal: document.querySelector("[data-modal-mob-buy-n]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
